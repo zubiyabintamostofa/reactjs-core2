6c531512562fbd2aec503ad1504a5e4555057edb
zubiyabintamostofa <zubiyabinte123@gmail.com>
2022-03-19 00:36:45 +0600

    four

--- src/App.js
+++ src/App.js
@@ -2,9 +2,41 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+const products=[
+  {
+    name:'laptop',price:80000},
+    {name:'phone',price:72000},
+    {name:'watch',price:8000},
+    {name:'tablet',price:30000}
+  
+]
+
   return (
     <div className="App">
+
+
+      {/* dynamic way */}
+
+     {
+        products.map(product=><Product name={product.name}  price={product.price}></Product>)
+     }
+
+
+      {/* <Product name="laptop" price="80000"></Product>
+      <Product name="phone"price="79000"></Product>
       
+      <Product name="watch"price="9000"></Product> */}
+
+      
+      
+    </div>
+  );
+}
+function Product(props){
+  return(
+    <div className="product">
+      <h3>Name:{props.name}</h3>
+      <p>Price:{props.price}</p>
     </div>
   )
 }
