a645222bf037e2c609956b803e2524b7ec3508db
zubiyabintamostofa <zubiyabinte123@gmail.com>
2022-03-19 15:50:11 +0600

    seven

--- src/App.js
+++ src/App.js
@@ -1,37 +1,75 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
-const products=[
-  {
-    name:'laptop',price:80000},
-    {name:'phone',price:72000},
-    {name:'watch',price:8000},
-    {name:'tablet',price:30000}
+
+
+
+
+
+//onno ekta way shikhar jonno aita comment kochi
+
+// const products=[
+//   {
+//     name:'laptop',price:80000},
+//     {name:'phone',price:72000},
+//     {name:'watch',price:8000},
+//     {name:'tablet',price:30000}
   
-]
+// ]
 
   return (
-    <div className="App">
-
-
-      {/* dynamic way */}
-
-     {
-        products.map(product=><Product name={product.name}  price={product.price}></Product>)
-     }
-
-
-      {/* <Product name="laptop" price="80000"></Product>
-      <Product name="phone"price="79000"></Product>
-      
-      <Product name="watch"price="9000"></Product> */}
-
-      
-      
+    <div className="App"> 
+      <Counter></Counter>
     </div>
   );
 }
+function Counter(){
+const [count,setCount] =useState(33);
+const increaseCount=()=>setCount(count+1);
+const decreaseCount=()=>setCount(count-1);
+
+
+
+
+// const increaseCount=()=>{
+//   const newCount=count+1;
+//   setCount(newCount);
+//}
+//console.log(abc)
+
+  return(
+    <div>
+      <h1>Count:{count}</h1>
+      <button onClick={increaseCount}>Increase</button>
+      <button onClick={decreaseCount}>Decrease</button>
+    </div>
+  )
+}
+
+
+
+
+
+
+
+
+
+
+/*{ eita return er por div eer vitorer angsho dynamic way te:
+
+
+//   products.map(product=><Product name={product.name}  price={product.price}></Product>)
+// }
+
+
+//  <Product name="laptop" price="80000"></Product>
+// <Product name="phone"price="79000"></Product>
+
+// <Product name="watch"price="9000"></Product>
+*/
+
 function Product(props){
   return(
     <div className="product">
