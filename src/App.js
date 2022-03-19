bc1a72101814cf0a30e185175500d211ebeb15c3
zubiyabintamostofa <zubiyabinte123@gmail.com>
2022-03-19 16:39:25 +0600

    nine

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
 
@@ -21,10 +21,42 @@ function App() {
 
   return (
     <div className="App"> 
-      <Counter></Counter>
+      {/* <Counter></Counter> */}
+      <ExternalUsers></ExternalUsers>
     </div>
   );
 }
+function ExternalUsers(){
+  const[users,setUsers]=useState([])
+  useEffect(()=>{
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res=>res.json())
+    .then(data=>setUsers(data))
+
+
+  },[])
+  return(
+    <div>
+      <h2>External Users</h2>
+      <p>{users.length}</p>
+      {
+        users.map(user=><Users name={user.name}email={user.email}></Users>)
+      }
+    </div>
+  )
+}
+function Users(props){
+  return(
+    <div style={{border:'2px solid pink',margin:'20px'}}>
+      <h3>Name:{props.name}</h3>
+      <p>Email:{props.email}</p>
+    </div>
+  )
+}
+
+
+
+
 function Counter(){
 const [count,setCount] =useState(33);
 const increaseCount=()=>setCount(count+1);
